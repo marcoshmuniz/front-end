8e67ea1c85fc3f8fe5a1a80b65fc50f526ec8e64
Marcos Muniz <vexxile@gmail.com>
2014-08-14 10:23:48 -0300

    teste

---
+++ client/src/main/java/com/stefanini/git/client/App.java
@@ -0,0 +1,13 @@
+package com.stefanini.git.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
